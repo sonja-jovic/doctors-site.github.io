81a7c27fe835c40fe3606c37f00d9ce871525dfd
Sonja <sonjajovic05@gmail.com>
2025-06-05 15:06:43 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <Container className="text-center mt-5">
+      <h1>Welcome to Catalyst Health</h1>
+      <p>Your Family’s Health Starts Here</p>
+      <Button variant="primary">Book an Appointment</Button>
+    </Container>
+  );
+}
+
+export default App;
